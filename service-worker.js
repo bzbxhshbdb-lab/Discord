8000e7bb267aee3896c97565c6e9ca4f22bc8e27
bzbxhshbdb-lab <bzbxhshbdb@gmail.com>
2026-01-22 20:02:59 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // sw.js — FIX DEFINITIVO
-const CACHE_NAME = "zxiter-cache-v11"; // MUDE A VERSÃO SEMPRE QUE ATUALIZAR
+const CACHE_NAME = "zxiter-cache-v12"; // MUDE A VERSÃO SEMPRE QUE ATUALIZAR
 
 const FILES_TO_CACHE = [
   "./",
